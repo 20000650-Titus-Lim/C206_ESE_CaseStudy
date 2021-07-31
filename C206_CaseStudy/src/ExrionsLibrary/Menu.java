015b22216a795b5eaf94a43bd298bf20f38ef7f6
20000650-Titus-Lim <20000650@myrp.edu.sg>
2021-07-31 12:32:31 +0800

    Basic Structure established in Main
    Added ExrionsLibrary for Helper, DBUtil, Validator and Menu.

---
+++ C206_CaseStudy/src/ExrionsLibrary/Menu.java
@@ -0,0 +1,54 @@
+package ExrionsLibrary;
+
+import java.util.ArrayList;
+
+/**
+* Menu helps to create menu driven programmes easily without typing it out all the time. 
+* Personal property of Titus Lim (Exrion)
+*
+* @author  Titus Lim
+* @version 1.1
+* @since   2021-05-17
+*/
+
+public class Menu {	
+	private static ArrayList<String> optionList = new ArrayList<String>();
+	
+	/**
+	* Prints a menu
+	* @param menu : An ArrayList, preferably from MenuObjects that holds the Strings to be printed
+	* @param title : The main title of the menu
+	*/
+	public static void printMenu(String title) {
+		printTitle(title);
+		for(int i = 0; i < optionList.size(); i++) {
+			System.out.printf("%d. %s\n", i + 1, optionList.get(i));
+		}
+		Helper.line(40, "-");
+	}
+	
+	/**
+	* Prints a title
+	* @param title : The title of this menu
+	*/
+	public static void printTitle(String title) {
+		Helper.line(40, "=");
+		System.out.println(title);
+		Helper.line(40, "=");
+	}
+	
+	/**
+	* Adds an option to the stored ArrayList
+	* @param option : String value to be stored in the ArrayList
+	*/
+	public void addOption(String option) {
+		optionList.add(new String(option));
+	}
+
+	/**
+	* Returns the stored ArrayList
+	*/
+	public ArrayList<String> getOptionList() {
+		return optionList;
+	}
+}
---
+++ C206_CaseStudy/src/renovationACE/renoACE_Main.java
@@ -0,0 +1,27 @@
+package renovationACE;
+
+import ExrionsLibrary.*;
+
+public class renoACE_Main {
+
+	public static void main(String[] args) {
+		renoACE_Main r = new renoACE_Main();
+		r.run();
+	}
+	
+	private void run() {
+		int choice = -1;
+		
+		switch(choice) {
+		case 1:
+			//TODO: Login for Users
+			break;
+		case 2:
+			//TODO: Login for Admins
+			break;
+		default:
+			System.out.println("Invalid Choice");
+		}
+	}
+
+}
