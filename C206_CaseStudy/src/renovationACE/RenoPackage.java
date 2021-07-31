2e8a7ad4c8285004a341091f47d1d19c40c7e68e
Titus Lim <20000650@myrp.edu.sg>
2021-07-31 14:05:27 +0800

    Updated RenoPackage with getters and constructor.

--- C206_CaseStudy/src/renovationACE/RenoPackage.java
+++ C206_CaseStudy/src/renovationACE/RenoPackage.java
@@ -7,4 +7,34 @@ public class RenoPackage {
 	private String desc;
 	private LocalDate startDate;
 	private LocalDate endDate;
+	private double amount;
+	
+	public RenoPackage(int code, String desc, LocalDate startDate, LocalDate endDate, double amount) {
+		super();
+		this.code = code;
+		this.desc = desc;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.amount = amount;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
 }
