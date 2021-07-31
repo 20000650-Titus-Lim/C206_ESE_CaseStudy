2a5071e56c6208dbd727d71ad7b65a0b25bb167c
Titus Lim <20000650@myrp.edu.sg>
2021-07-31 12:47:39 +0800

    Main Login menu created

--- C206_CaseStudy/src/renovationACE/renoACE_Main.java
+++ C206_CaseStudy/src/renovationACE/renoACE_Main.java
@@ -3,6 +3,8 @@ package renovationACE;
 import ExrionsLibrary.*;
 
 public class renoACE_Main {
+	//Main Menu (Login)
+	private Menu menLogin = new Menu();
 
 	public static void main(String[] args) {
 		renoACE_Main r = new renoACE_Main();
@@ -10,19 +12,29 @@ public class renoACE_Main {
 	}
 	
 	private void run() {
+		genMenuLogin();
 		int choice = -1;
 		
-		switch(choice) {
-		case 1:
-			//TODO: Login for Users
-			runUser();
-			break;
-		case 2:
-			//TODO: Login for Admins
-			runAdmin();
-			break;
-		default:
-			System.out.println("Invalid Choice");
+		while(choice != 3) {
+			menLogin.printMenu("LOGIN");
+			choice = Validator.readIntPos("Enter choice > ");
+			
+			switch(choice) {
+			case 1:
+				//TODO: Login for Users
+				runUser();
+				break;
+			case 2:
+				//TODO: Login for Admins
+				runAdmin();
+				break;
+			case 3:
+				//Terminate Program
+				System.out.println("App exited successfully");
+				break;
+			default:
+				System.out.println("Invalid Choice");
+			}
 		}
 	}
 	
@@ -32,6 +44,12 @@ public class renoACE_Main {
 	
 	private void runAdmin() {
 		
+	}
+	
+	private void genMenuLogin() {
+		menLogin.addOption("User Login");
+		menLogin.addOption("Admin Login");
+		menLogin.addOption("Quit");
 	}
 
 }
